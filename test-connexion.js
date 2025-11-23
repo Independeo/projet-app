d6374d1a762afe4a65fffc466bd3b7d6eae0e802
David Katukani <katukanid@gmail.com>
2025-11-23 16:33:36 +0100

    update project

---
+++ test-connexion.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connexion réussie !");
+  } catch (e) {
+    console.error("Erreur de connexion :", e);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main();
